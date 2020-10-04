7fcab730705b572d8fc81afb3f8240eccaf6358d
mdtro <matt@matttrostel.com>
2020-10-04 16:36:47 -0500

    test for cheese get_absolute_url

--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -1,7 +1,5 @@
 import pytest
-
 from .factories import CheeseFactory
-from ..models import Cheese
 
 pytestmark = pytest.mark.django_db
 
@@ -10,3 +8,9 @@ def test__str__():
     cheese = CheeseFactory()
     assert cheese.__str__() == cheese.name
     assert str(cheese) == cheese.name
+
+
+def test_get_absolute_url():
+    cheese = CheeseFactory()
+    url = cheese.get_absolute_url()
+    assert url == f'/cheeses/{cheese.slug}'
