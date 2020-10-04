5c02e5442512830e9d56db22949e73181a11b864
mdtro <matt@matttrostel.com>
2020-10-04 15:18:15 -0500

    add cheese functionality

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 from autoslug import AutoSlugField
 from model_utils.models import TimeStampedModel
@@ -7,7 +8,6 @@ from django_countries.fields import CountryField
 
 
 class Cheese(TimeStampedModel):
-
     class Firmness(models.TextChoices):
         UNSPECIFIED = "unspecified", "Unspecified"
         SOFT = "soft", "Soft"
@@ -16,10 +16,20 @@ class Cheese(TimeStampedModel):
         HARD = "hard", "Hard"
 
     name = models.CharField("Name of Cheese", max_length=255)
-    slug = AutoSlugField("Cheese address", unique=True, always_update=False, populate_from="name")
+    slug = AutoSlugField(
+        "Cheese address", unique=True, always_update=False, populate_from="name"
+    )
     description = models.TextField("Description", blank=True)
-    firmness = models.CharField("Firmness", max_length=20, choices=Firmness.choices, default=Firmness.UNSPECIFIED)
+    firmness = models.CharField(
+        "Firmness",
+        max_length=20,
+        choices=Firmness.choices,
+        default=Firmness.UNSPECIFIED,
+    )
     country_of_origin = CountryField("Country of Origin", blank=True)
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("cheeses:detail", kwargs={"slug": self.slug})
